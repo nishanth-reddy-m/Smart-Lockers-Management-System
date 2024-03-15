5d5c4c77e36ef5e2c3322e18ec30f34350da911e
Nishanth <nishanth.reddym004@gmail.com>
2024-03-15 16:01:02 +0530

    Interface & Backend Upgrade

--- app.py
+++ app.py
@@ -29,66 +29,64 @@ def home():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
-    if request.method == 'POST':
-        db = mongo.majorproject
-        if 'sendotp' in request.form:
-            userid=request.form['userid']
-            session['userid']=userid
-            exists=db.users.find_one({'userid':userid})
-            if exists:
-                flash('User already exists')
-                return render_template('register.html',userid=userid, condition2='disabled')
-            else:
-                userid=session['userid']
-                otp = str(random.randint(100000,999999))
-                message = f'Your otp for verification is {otp}'
-                session['otp']=otp
-                msg = Message(subject='Verification',
-                      recipients=[userid],
-                      body=message,
-                      extra_headers={"Importance": "High"}
-                )
-                try:
-                    mail.send(msg)
-                    flash('OTP Sent')
-                    return render_template('register.html',userid=userid)
-                except Exception as e:
-                    flash(f'Failed to send email: {str(e)}')
-                    return render_template('register.html',userid=userid)
-                    
-        elif 'verifyotp' in request.form:
-            entered_otp=request.form['otp']
-            otp=session['otp']
-            if(entered_otp == otp):
-                userid = session['userid']
-                flash('Verification Successful')
-                return render_template('register.html',userid=userid,condition1='disabled',condition2='disabled',verified=True)
-            else:
-                userid=session['userid']
-                flash('Incorrect OTP. Please try again')
+    db = mongo.majorproject
+    if 'sendotp' in request.form:
+        userid=request.form['userid']
+        session['userid']=userid
+        exists=db.users.find_one({'userid':userid})
+        if exists:
+            flash('User already exists')
+            return render_template('register.html',userid=userid, condition2='disabled')
+        else:
+            userid=session['userid']
+            otp = str(random.randint(100000,999999))
+            message = f'Your otp for verification is {otp}'
+            session['otp']=otp
+            msg = Message(subject='Verification',
+                    recipients=[userid],
+                    body=message,
+                    extra_headers={"Importance": "High"}
+            )
+            try:
+                mail.send(msg)
+                flash('OTP Sent')
                 return render_template('register.html',userid=userid)
-        elif 'register' in request.form:
+            except Exception as e:
+                flash(f'Failed to send email: {str(e)}')
+                return render_template('register.html',userid=userid)
+    elif 'verifyotp' in request.form:
+        entered_otp=request.form['otp']
+        otp=session['otp']
+        if(entered_otp == otp):
             userid = session['userid']
-            username = request.form['username']
-            userpass = request.form['userpass']
-            checkpass = request.form['checkpass']
-            q1 = request.form['question1']
-            a1 = request.form['answer1'].lower().replace(" ", "")
-            q2 = request.form['question2']
-            a2 = request.form['answer2'].lower().replace(" ", "")
-            q3 = request.form['question3']
-            a3 = request.form['answer3'].lower().replace(" ", "")
-            db.users.insert_one({ 
-                'userid':userid, 
-                'userpass':userpass,
-                'username':username,
-                'qanda':{q1:a1, q2:a2, q3:a3},
-                'checked_in':[],
-                'locker_log':{}, 
-                'wallet':0 })
-            session.pop('userid', None)
-            session.pop('otp', None)
-            return render_template('registrationsuccess.html')
+            flash('Verification Successful')
+            return render_template('register.html',userid=userid,condition1='disabled',condition2='disabled',verified=True)
+        else:
+            userid=session['userid']
+            flash('Incorrect OTP. Please try again')
+            return render_template('register.html',userid=userid)
+    elif 'register' in request.form:
+        userid = session['userid']
+        username = request.form['username']
+        userpass = request.form['userpass']
+        checkpass = request.form['checkpass']
+        q1 = request.form['question1']
+        a1 = request.form['answer1'].lower().replace(" ", "")
+        q2 = request.form['question2']
+        a2 = request.form['answer2'].lower().replace(" ", "")
+        q3 = request.form['question3']
+        a3 = request.form['answer3'].lower().replace(" ", "")
+        db.users.insert_one({ 
+            'userid':userid, 
+            'userpass':userpass,
+            'username':username,
+            'qanda':{q1:a1, q2:a2, q3:a3},
+            'checked_in':[],
+            'locker_log':{}, 
+            'wallet':0 })
+        session.pop('userid', None)
+        session.pop('otp', None)
+        return render_template('registrationsuccess.html')
     flash('Not yet Verified')
     return render_template('register.html',condition2='disabled')
 
@@ -115,9 +113,8 @@ def public_disabled(locker):
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
+        db = mongo.majorproject
         if 'login' in request.form:
-            db = mongo.majorproject
             userid = request.form['userid']
             userpass = request.form['userpass']
             user=db.users.find_one({'userid':userid,'userpass':userpass})
@@ -125,11 +122,30 @@ def login():
                 session['userid'] = userid
                 dblockers = db.lockers.find_one({},{'_id': 0, 'all_lockers': 1})
                 lockers = dblockers['all_lockers']
+                session['lockers'] = lockers
                 return render_template('interface.html',lockers=lockers,private_disabled=private_disabled,public_disabled=public_disabled)
             else:
                 flash('Invalid UserID or Password')
                 return render_template('login.html', userid=userid)
-    return render_template('login.html')
+        if 'checkin' in request.form:
+            userid = session['userid']
+            lockers = session['lockers']
+            checked_in = request.form.getlist('global_lockers')
+            db.users.update_one({'userid':userid}, {"$push": {"checked_in": {"$each": checked_in}}})
+            db.lockers.update_one({},{'$pull':{'available_lockers':{'$in': checked_in}}})
+            return render_template('interface.html',lockers=lockers,private_disabled=private_disabled,public_disabled=public_disabled)
+        if 'checkout' in request.form:
+            userid = session['userid']
+            lockers = session['lockers']
+            checked_in = request.form.getlist('user_lockers')
+            db.users.update_one({'userid':userid}, {"$pull": {"checked_in":{'$in': checked_in}}})
+            db.lockers.update_one({},{'$push':{'available_lockers':{'$each': checked_in}}})
+            return render_template('interface.html',lockers=lockers,private_disabled=private_disabled,public_disabled=public_disabled)
+        if 'logout' in request.form:
+            session.pop('userid', None)
+            session.pop('lockers', None)
+            return redirect('/')
+        return render_template('login.html')
 
 @app.route('/reset', methods=['GET','POST'])
 def reset():
